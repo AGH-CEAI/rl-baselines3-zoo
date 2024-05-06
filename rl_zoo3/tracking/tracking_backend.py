44c7e4816ddba5e70420b656fd8f1238d7932956
macmacal <mac.aleksandrowicz@gmail.com>
2024-05-06 17:25:46 +0200

    changed print to logging; added more verbose warning information

--- rl_zoo3/tracking/tracking_backend.py
+++ rl_zoo3/tracking/tracking_backend.py
@@ -1,14 +1,17 @@
-import os
 import argparse
-from typing import Dict, Any, Set
-
+import logging
+import os
 from abc import ABC, abstractmethod
-from rl_zoo3.tracking.git import track_git_repos
+from typing import Dict, Any, Set
 
 import rl_zoo3
 import sb3_contrib
 import stable_baselines3 as sb3
+from rl_zoo3.tracking.git import track_git_repos
 from stable_baselines3.common.logger import Logger
+
+
+logger = logging.getLogger(__name__)
 
 
 class TrackingBackend(ABC):
@@ -82,7 +85,7 @@ class TrackingBackend(ABC):
 
     def get_sb3_logger(self, verbose: bool) -> Logger | None:
         if not self.is_initalized():
-            print(
+            logger.warning(
                 f"WARNING: Tried to obtain SB3 Logger without initialization of the tracking backend! Falling back to defaults."
             )
             return None
@@ -90,6 +93,9 @@ class TrackingBackend(ABC):
 
     def log_params(self, params: Dict | Any) -> None:
         if not self.is_initalized():
+            logger.warning(
+                f"WARNING: Tried to log paramaters without initialization of the tracking backend! Call ignored."
+            )
             return
 
         params_dict = params
@@ -100,6 +106,9 @@ class TrackingBackend(ABC):
 
     def log_directory(self, local_dir: str, artifacts_dir: str = None) -> None:
         if not self.is_initalized():
+            logger.warning(
+                f"WARNING: Tried to log directory without initialization of the tracking backend! Call ignored."
+            )
             return
         self._log_directory(local_dir=local_dir, artifacts_dir=artifacts_dir)
 
