fdf579844945a150e4dbc096097cdda1ccbd74c7
macmacal <mac.aleksandrowicz@gmail.com>
2024-05-16 18:24:37 +0200

    updated doc strings

--- rl_zoo3/feature_extractors.py
+++ rl_zoo3/feature_extractors.py
@@ -15,8 +15,14 @@ from stable_baselines3.common.preprocessing import is_image_space
 
 class AbstractVisionExtractor(BaseFeaturesExtractor, ABC):
     """
-    Load pre-trained model from torchvision library as feature extractor.
-    List of avaiable models: https://pytorch.org/vision/main/models.html
+    An abstract class for preparing pre-defined vision feature extractors,
+    like CNN, VAE, ViT, M. Hopfield etc.
+
+    It adds an extra nn.Linear layer with the selected activation function for
+    scaling the output number of features.
+
+    If the observation consists of stacked frames, these frames will be split
+    and given to the feature extractor separately.
 
     :param observation_space:
     :param normalized_image: Whether to assume that the image is already normalized
@@ -24,24 +30,25 @@ class AbstractVisionExtractor(BaseFeaturesExtractor, ABC):
         the space is a Box and has 3 dimensions.
         Otherwise, it checks that it has expected dtype (uint8)
         and bounds (values in [0, 255]).
-    :param model_name: the name of the model in the PascalCase format.
-    :param weights_id: the name of the trained weights (torchvision API).
-    :param cut_on_layer: the name of the layer to cut the head from the backbone.
-    :param add_linear_layer: enables creation of linear layer
-        for enforcing output dimension.
-    :param linear_features_dim: Number of features extracted.
-            This corresponds to the number of unit for the last layer.
-    :param linear_activation_fn: The activation function to use after each layer.
+    :param out_features_per_frame: Number of extracted features from a single image.
+        This corresponds to the number of unit in the last layer.
+        If the frames are stacked, the output dimension will be equal to
+        out_features_per_frame * stacking_frames.
+    :param linear_activation_fn: The activation function to use after the last layer.
+    :param stacking_frames: The number of the stacked frames.
+        Must be the same as the hyperparamter `frame_stack`.
+    :param frame_channels: The number of color channels in the input images.
+        Typically (1) for monochromatic, (3) for RGB and (4) for RGBD.
     """
 
     def __init__(
         self,
         observation_space: gym.Space,
-        normalized_image: bool,
         out_features_per_frame: int,
         linear_activation_fn: Type[nn.Module],
         stacking_frames: int | None,
         frame_channels: int,
+        normalized_image: bool,
     ):
         self._validate_params(observation_space, normalized_image, stacking_frames)
         if self.is_single_frame:
@@ -105,24 +112,47 @@ class AbstractVisionExtractor(BaseFeaturesExtractor, ABC):
 
 
 class CustomVisionExtractor(AbstractVisionExtractor):
+    """
+    Wraps user defined model for usage as visual feature extractor.
+    
+    Check AbstractVisionExtractor for more information.
+
+    :param observation_space:
+    :param custom_feature_extractor: the nn.Module defined by the user.
+    :param out_features_per_frame: Number of extracted features from a single image.
+        This corresponds to the number of unit in the last layer.
+        If the frames are stacked, the output dimension will be equal to
+        out_features_per_frame * stacking_frames.
+    :param linear_activation_fn: The activation function to use after the last layer.
+    :param stacking_frames: The number of the stacked frames.
+        Must be the same as the hyperparamter `frame_stack`.
+    :param frame_channels: The number of color channels in the input images.
+        Typically (1) for monochromatic, (3) for RGB and (4) for RGBD.
+    :param normalized_image: Whether to assume that the image is already normalized
+        or not (this disables dtype and bounds checks): when True, it only checks that
+        the space is a Box and has 3 dimensions.
+        Otherwise, it checks that it has expected dtype (uint8)
+        and bounds (values in [0, 255]).
+    """
+    
     def __init__(
         self,
         observation_space: gym.Space,
         custom_feature_extractor: Type[nn.Module],
-        normalized_image: bool = False,
         out_features_per_frame: int = 512,
         linear_activation_fn: Type[nn.Module] = nn.ReLU,
         stacking_frames: int = None,
         frame_channels: int = 3,
+        normalized_image: bool = False,
     ):
         self._custom_model = custom_feature_extractor
         super().__init__(
-            observation_space,
-            normalized_image,
-            out_features_per_frame,
-            linear_activation_fn,
-            stacking_frames,
-            frame_channels,
+            observation_space=observation_space,
+            out_features_per_frame=out_features_per_frame,
+            linear_activation_fn=linear_activation_fn,
+            stacking_frames=stacking_frames,
+            frame_channels=frame_channels,
+            normalized_image=normalized_image,
         )
 
     def _prepare_feature_extractor(self):
@@ -130,29 +160,55 @@ class CustomVisionExtractor(AbstractVisionExtractor):
 
 
 class PreTrainedVisionExtractor(AbstractVisionExtractor):
+    """
+    Loads a pre-trained model from the torchvision library as feature extractor.
+    List of avaiable models: https://pytorch.org/vision/main/models.html .
+    
+    Check AbstractVisionExtractor for more information.
+
+    :param observation_space:
+    :param model_name: the name of the model in the PascalCase format.
+    :param weights_id: the name of the trained weights (torchvision API).
+    :param cut_on_layer: the name of the layer to cut the head from the backbone.
+    :param out_features_per_frame: Number of extracted features from a single image.
+        This corresponds to the number of unit in the last layer.
+        If the frames are stacked, the output dimension will be equal to
+        out_features_per_frame * stacking_frames.
+    :param linear_activation_fn: The activation function to use after the last layer.
+    :param stacking_frames: The number of the stacked frames.
+        Must be the same as the hyperparamter `frame_stack`.
+    :param frame_channels: The number of color channels in the input images.
+        Typically (1) for monochromatic, (3) for RGB and (4) for RGBD.
+    :param normalized_image: Whether to assume that the image is already normalized
+        or not (this disables dtype and bounds checks): when True, it only checks that
+        the space is a Box and has 3 dimensions.
+        Otherwise, it checks that it has expected dtype (uint8)
+        and bounds (values in [0, 255]).
+    """
+
     def __init__(
         self,
         observation_space: gym.Space,
         model_name: str = None,
         weights_id: str | None = None,
         cut_on_layer: str = None,
-        normalized_image: bool = False,
         out_features_per_frame: int = 512,
         linear_activation_fn: Type[nn.Module] = nn.ReLU,
         stacking_frames: int = None,
         frame_channels: int = 3,
+        normalized_image: bool = False,
     ):
         self._import_torchvision()
         self._model_name = model_name
         self._weights_id = weights_id
         self._cut_on_layer = cut_on_layer
         super().__init__(
-            observation_space,
-            normalized_image,
-            out_features_per_frame,
-            linear_activation_fn,
-            stacking_frames,
-            frame_channels,
+            observation_space=observation_space,
+            out_features_per_frame=out_features_per_frame,
+            linear_activation_fn=linear_activation_fn,
+            stacking_frames=stacking_frames,
+            frame_channels=frame_channels,
+            normalized_image=normalized_image,
         )
 
     def _import_torchvision(self):
